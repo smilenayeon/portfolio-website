81f1346081c1644f18fc817d4b26787480faeadf
Na Yeon <smilenayeon@naver.com>
2023-12-28 07:09:07 +0900

    git new project react-redux task tracker updated

--- src/components/pages/projects/ProjectsData.js
+++ src/components/pages/projects/ProjectsData.js
@@ -6,6 +6,7 @@ import Weather from  "../../../assets/images/weather.jpg";
 import MovieSearch from "../../../assets/images/movie-search.png";
 import AdventCalendar from "../../../assets/images/AdventCalendat.png";
 import NewsLetterSignUp from "../../../assets/images/newsletter-signup.jpg";
+import TaskTracker from"../../../assets/images/tasktracker.png";
 
 class Project  {
   image="";
@@ -25,9 +26,11 @@ const ProjectData = [
     new Project(AdventCalendar, "Advent Calendar", "https://smilenayeon.github.io/Advent-Calendar/","https://github.com/smilenayeon/Advent-Calendar" ),
     new Project(NewsLetterSignUp, "Newsletter sign-up form", "https://smilenayeon.github.io/newsletter-signup/","https://github.com/smilenayeon/newsletter-signup.git" ),
     new Project(Weather, "Simply Weather", "", "https://github.com/smilenayeon/Simply-Weather.git"),
+    new Project(TaskTracker, "React-Redux Task Tracker", "https://dianago-task-tracker.netlify.app/", "https://github.com/smilenayeon/task-tracker.git"),
     new Project(Todo, "To-Do List", "https://dianago-todolist.netlify.app/", "https://github.com/smilenayeon/todo-list.git"),
     new Project(MovieSearch, "Movie Search", "https://dianago-movie-search.netlify.app/","https://github.com/smilenayeon/movie-website.git" ),
     new Project(Game, "ESL Card Game", "https://dianago-esl-game.netlify.app", "https://github.com/smilenayeon/game.git"),
+    
 ];
 
 export default ProjectData;
