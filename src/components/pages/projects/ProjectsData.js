beda0deba8ed644ee189036395d0d88e326244d5
Na Yeon <smilenayeon@naver.com>
2024-01-03 21:34:48 +0900

    interactive credit card project is added

--- src/components/pages/projects/ProjectsData.js
+++ src/components/pages/projects/ProjectsData.js
@@ -6,7 +6,8 @@ import Weather from  "../../../assets/images/weather.jpg";
 import MovieSearch from "../../../assets/images/movie-search.png";
 import AdventCalendar from "../../../assets/images/AdventCalendat.png";
 import NewsLetterSignUp from "../../../assets/images/newsletter-signup.jpg";
-import TaskTracker from"../../../assets/images/tasktracker.png";
+import TaskTracker from "../../../assets/images/tasktracker.png";
+import CreditCard from "../../../assets/images/creditcard.jpg";
 
 class Project  {
   image="";
@@ -24,9 +25,10 @@ class Project  {
 const ProjectData = [
     new Project(DesignWeb, "Web-Design Company Website","https://dianago-web-design-website.netlify.app", "https://github.com/smilenayeon/Web-Design-Company-Website.git"),
     new Project(AdventCalendar, "Advent Calendar", "https://smilenayeon.github.io/Advent-Calendar/","https://github.com/smilenayeon/Advent-Calendar" ),
-    new Project(NewsLetterSignUp, "Newsletter sign-up form", "https://smilenayeon.github.io/newsletter-signup/","https://github.com/smilenayeon/newsletter-signup.git" ),
+    new Project()
     new Project(Weather, "Simply Weather", "", "https://github.com/smilenayeon/Simply-Weather.git"),
     new Project(TaskTracker, "React-Redux Task Tracker", "https://dianago-task-tracker.netlify.app/", "https://github.com/smilenayeon/task-tracker.git"),
+    new Project(NewsLetterSignUp, "Newsletter sign-up form", "https://smilenayeon.github.io/newsletter-signup/","https://github.com/smilenayeon/newsletter-signup.git" ),
     new Project(Todo, "To-Do List", "https://dianago-todolist.netlify.app/", "https://github.com/smilenayeon/todo-list.git"),
     new Project(MovieSearch, "Movie Search", "https://dianago-movie-search.netlify.app/","https://github.com/smilenayeon/movie-website.git" ),
     new Project(Game, "ESL Card Game", "https://dianago-esl-game.netlify.app", "https://github.com/smilenayeon/game.git"),
